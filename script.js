8ecec709ef688a6c599ce719fada0edf8973d0bb
ahmed-mgd <ahmedg.abdel@gmail.com>
2023-07-23 16:29:46 -0400

    Implemented getComputerChoice

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 function getComputerChoice() {
+    switch(Math.random()) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3: 
+            return "scissors";
+        default:
+            console.error("ERROR: Computer choice out of bounds");
+    }
 
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+
 }
 
 function game() {
