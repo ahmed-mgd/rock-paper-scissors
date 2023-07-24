1b1e4967f92a3318ddc1a6799348f1792214a23c
ahmed-mgd <ahmedg.abdel@gmail.com>
2023-07-23 20:44:07 -0400

    Roughly implemented methods

--- script.js
+++ script.js
@@ -13,9 +13,25 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-
+    // Set player as winner by default. Set to false if any losing case or tie is met.
+    let playerWin = true;
+    const playerSelectionLC = playerSelection.toLowerCase;
+    if (playerSelection.localeCompare(computerSelection, underfined, { sensitivity: 'accent'} ) == 0) {
+        return `Tie! Both parties chose ${playerSelection}`;
+    } else {
+    }
+    if (playerWin) {
+        return `You win! ${playerSelection} beats ${computerSelection}`; 
+    } else {
+        return `You lose! ${computerSelection} beats ${computerSelection}`; 
+    }
 }
 
 function game() {
-
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Enter your choice (rock, paper, or scissors): ");
+        playRound(playerSelection, getComputerChoice());
+    }
 }
+
+game();
